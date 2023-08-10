e1d3b45e56825eadad6bc2f468c037b051b7a119
@undrokisel <undrokisel@gmail.com>
2023-08-10 15:29:03 +0300

    added favorite

--- src/App.js
+++ src/App.js
@@ -42,10 +42,20 @@ function App() {
       {/* ------------------------------------------------------ */}
 
       <section className="goods">
-        <h3 className="goods__title">All perfumes</h3>
+        <div className="goods__top">
+          <h3 className="goods__title">All perfumes</h3>
+          <div className="goods__search-block">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+            <input type="search" placeholder="Search ..." />
+          </div>
+        </div>
         <div className="goods__cards">
 
+
           <div className="goods__card card">
+
+            <div className="favorite favorite_unchecked">
+            </div>
             <div className="card__img">
               <img
                 width={133}
@@ -126,7 +136,7 @@ function App() {
             </div>
           </div>
 
-       
+
 
         </div>
       </section>
