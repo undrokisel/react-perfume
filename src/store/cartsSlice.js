25232d62824ba9689c21abb528f316a2c8bfe6e4
@undrokisel <undrokisel@gmail.com>
2023-09-18 17:38:46 +0300

    fixed total count in slice with remove item from cart

--- src/store/cartsSlice.js
+++ src/store/cartsSlice.js
@@ -5,7 +5,7 @@ export const fetchCart = createAsyncThunk(
     'cart/fetch',
     async (_, { rejectWithValue, dispatch, }) => {
         try {
-            const response = await instance.get(URL_CART) 
+            const response = await instance.get(URL_CART)
             if (response.status && response.status !== 200) {
                 throw new Error("Error with fetching cart")
             }
@@ -60,7 +60,7 @@ export const removeFromCartThunk = createAsyncThunk(
 const setError = (state, action) => {
     state.status = 'rejected'
     state.error = action.payload
-    console.log('error'); 
+    console.log('error');
 }
 const setLoading = (state) => {
     state.status = 'loading'
@@ -68,7 +68,7 @@ const setLoading = (state) => {
 }
 
 const calcTotal = (items) => {
-    let total = items.reduce((total, item) => total += +item.price, 0)
+    let total = items.reduce((total, item) => total = +total + +(item.price), 0)
     return total.toFixed(2)
 }
 
@@ -88,7 +88,8 @@ const cartsSlice = createSlice({
         removeFromCart(state, action) {
             const priceItem = state.list.find(item => item.id === action.payload).price
             state.list = state.list.filter(item => item.id !== action.payload)
-            state.total = +state.total - +priceItem.toFixed(2)
+            state.total = Number(state.total) - ((Number(priceItem)).toFixed(2))
+            console.log(state.total);
         }
     },
     extraReducers: {
